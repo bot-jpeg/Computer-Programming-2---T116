ae060dd3a32bce42844e5ae8606384b2c19c30a6
Jayson T <61448633+bot-jpeg@users.noreply.github.com>
2022-03-18 10:20:23 +0800

    Update TheNthPower.c

--- Activity 1.0  - Crash Course/TheNthPower.c
+++ Activity 1.0  - Crash Course/TheNthPower.c
@@ -1,40 +1,35 @@
+//TADAYCA, JAYSON L.
 #include <stdio.h>
- 
-int main()
-{
-    int base, exponent, expo;
-    float power = 1;
-    int i;
- 
-    /* Take base and exponent as input*/
-    printf(" X: ");
-    scanf("%d", &base);
-    printf(" Y: ");
-    scanf("%d", &exponent);
-    
-    expo = exponent;
-    
-    if(exponent >0)
-    {
-        /*Calculate power */
-        while (exponent!=0)
-        {
-            power = power * base;
-            exponent--;
-        }
-        printf(" %d ^ %d = %.0f", base, exponent, power);
+
+int main() {
+  int base, exponent, expo;
+  float power = 1;
+  int i;
+
+  /* Take base and exponent as input*/
+  printf(" X: ");
+  scanf("%d", & base);
+  printf(" Y: ");
+  scanf("%d", & exponent);
+
+  expo = exponent;
+
+  if (exponent > 0) {
+    /*Calculate power */
+    while (exponent != 0) {
+      power = power * base;
+      exponent--;
     }
-    else
-    {
-        while (expo != 0)
-        {
-            {
-            power = power/base;
-            expo++;
-                
-            }
-        }
-        printf(" %d ^ %d = %.3f", base, exponent, power);
+    printf(" %d ^ %d = %.0f", base, expo, power);
+  } else {
+    while (expo != 0) {
+      {
+        power = power / base;
+        expo++;
+
+      }
     }
-    return 0;
+    printf(" %d ^ %d = %.3f", base, exponent, power);
+  }
+  return 0;
 }
